df316e969f655acc7f6d03c9a09f201586ac0b58
Fernando Nogueira <abert036@uottawa.ca>
2023-03-20 02:58:22 -0400

    chore: update install_symlinks.py

--- bootstrap/install_symlinks.py
+++ bootstrap/install_symlinks.py
@@ -35,7 +35,7 @@ for root, dirnames, filenames in os.walk('.'):
         if filename in ignore_files:
             continue
         # Check if file matches any of the ignore patterns
-        if any([i.match(os.path.join(root, filename)) for i in ignore]):
+        if any(i.match(os.path.join(root, filename)) for i in ignore):
             continue
         files.append(os.path.join(root, filename))
 
@@ -43,16 +43,59 @@ print("Filtering files...")
 
 
 # Prompt every file to be symlinked
+ctx = ""
 for f in files:
+    print("--------------------")
+    print()
     print("####################")
     print(f"File: {f}")
     print("####################")
     # Check if the file exists relative to the home directory
     if os.path.exists(os.path.expanduser('~/' + f)):
-        print("File already exists, skipping...")
-        continue
+
+        if ctx == '3' or ctx == '4':
+            print("Skipping all...")
+            continue
+
+        print("File already exists, would you like to overwrite it?")
+        print("1. Yes")
+        print("2. No")
+        print("3. Skip all existing files")
+        print("4. Overwrite all existing files")
+        print("5. Exit")
+        while True:
+            ctx = input("Enter option: ")
+            if ctx in ['1', '2', '3', '4', '5']:
+                break
+            print("Invalid option, try again")
+
+        if ctx == '1':
+            print("Overwriting file...")
+            os.remove(os.path.expanduser('~/' + f))
+        elif ctx == '2':
+            print("Skipping...")
+            continue
+        elif ctx == '3':
+            print("Skipping all...")
+            break
+        elif ctx == '4':
+            print("Overwriting all...")
+            os.remove(os.path.expanduser('~/' + f))
+        elif ctx == '5':
+            print("Exiting...")
+            sys.exit(0)
+
+    if ctx == '4':
+        print("Overwriting all...")
+    elif ctx == '1':
+        print("Overwriting file...")
     else:
         print("File does not exist, creating symlink...")
-        # Create symlink
-        break
-        # os.symlink(os.path.abspath(f), os.path.expanduser('~/' + f))
+        try:
+            input("Press enter to proceed (Ctrl+C to skip file))")
+        except KeyboardInterrupt:
+            print("Skipping...")
+            continue
+
+    # Create symlink
+    os.symlink(os.path.abspath(f), os.path.expanduser('~/' + f))
